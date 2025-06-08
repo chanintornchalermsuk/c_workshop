ff45125ceceb38854e329218188d8c82727f8f58
chanintornchalermsuk <chanintornchalermsuk@gmail.com>
2025-06-08 11:50:22 +0700

    แV.3 แก้ 07 แล้วก็ทดสอบ

--- lecture07.c
+++ lecture07.c
@@ -1,5 +1,19 @@
+//ตัวแปร กับ format code
 #include <stdio.h>
 
 void main(){
-    printf("Welcome to IoT-SAU");
+    //สร้างตัวแปรต้องมี   ชนิดข้อมูล   ชื่อตัวแปร;
+    int aa = 1000000;  //(%d) จำนวนเต็มไม่เกินหลักล้าน
+    float bb = 20.525;   //(%f) ทศนิยมไม่เกิน 6 หลัก
+    double cc = 215.549794565;  //(%lf)ทศนิยมเกิน 6 หลัก
+    char dd = '#';  //(%c) ตัวอักษร Charactor คือ ตัวอักษรตัวเดียว อยู่ใน '?'
+    char ee[] = "Hello...^_^";    //(%s) ข้อความ String คือ ตัวอักษรตั้งแต่ 0 ตัวขึ้นไป อยู่ใน "?????"
+    long ff = 10000000; //(%ld)จำนวนเต็มเกินหลักล้านแต่อย่าเกิดพันล้าน
+
+    printf("AA : %d", aa);
+    printf("\nBB : %f", bb);
+    printf("\nCC : %lf", cc);
+    printf("\nDD : %c", dd);
+    printf("\nEE : %s", ee);
+    printf("\nFF : %ld", ff);
 }
